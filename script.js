0e1c5a114bdf4950f5f053a75406dab44e56475e
Mario Krok-Adamski <mariokadamski@Marios-MacBook-Air.local>
2021-06-23 16:26:02 +0200

    added theme switcher and fixed mobile version

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.switcher input');
+const span = document.querySelector('.switcher span');
+
+
+
+input.addEventListener('change', (e) => {
+  result = (e.target.checked) ?
+    document.body.setAttribute('data-theme', 'light')  :
+    document.body.setAttribute('data-theme', 'dark')
+});
