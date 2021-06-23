cfde931be521b7b5aac06f0e5bbfaf62b258b652
Mario Krok-Adamski <mariokadamski@Marios-MacBook-Air.local>
2021-06-23 22:26:01 +0200

    added localstorage

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const input = document.querySelector('.switcher input');
+const currentTheme = localStorage.getItem('data-theme');
 
+if (currentTheme) {
+  document.body.setAttribute('data-theme', currentTheme);
 
+    if (currentTheme === 'light') {
+      input.checked = true;
+      document.body.dataset.theme == 'light';
+    }
+}
 
 input.addEventListener('change', (e) => {
-  result = (e.target.checked) ?
-    document.body.setAttribute('data-theme', 'light')  :
-    document.body.setAttribute('data-theme', 'dark')
+  if(e.target.checked) {
+    document.body.setAttribute('data-theme', 'light');
+    localStorage.setItem('data-theme', 'light');
+  } else {
+    document.body.setAttribute('data-theme', 'dark');
+    localStorage.setItem('data-theme', 'dark');
+  }
 });
+
+
 
